70507cc7fd3f541e8ec824b27112b49e9c9d5311
Emma Kordik <emma@gbsvoice.com>
2018-11-29 19:19:08 -0600

    Class from 11/29 Started adding functionality for multiline programs

--- app/src/main/java/ekordik/cuw/edu/arminterpreter/DecimalInstructionTranslationServiceImpl.java
+++ app/src/main/java/ekordik/cuw/edu/arminterpreter/DecimalInstructionTranslationServiceImpl.java
@@ -6,19 +6,20 @@ public class DecimalInstructionTranslationServiceImpl implements InstructionTran
 
     @Override
     public void executeInstruction(Instruction instruction) {
-        Register destinationRegister = ARMap.findRegisterWithName(instruction.getDestinationName());
+        Register destinationRegister = ARMap.findRegisterWithName(instruction.getParameters()[0]);
 
         String magicTrick = instruction.getMagicTrick().toUpperCase();
         switch(magicTrick) {
             case "ADD":
-                Register input1Register = ARMap.findRegisterWithName(instruction.getInputNames().get(0));
-                Register input2Register = ARMap.findRegisterWithName(instruction.getInputNames().get(1));
+                Register input1Register = ARMap.findRegisterWithName(instruction.getParameters()[1]);
+                Register input2Register = ARMap.findRegisterWithName(instruction.getParameters()[2]);
 
                 destinationRegister.setValue(input1Register.getValue() + input2Register.getValue());
                 break;
             case "SUB":
-                input1Register = ARMap.findRegisterWithName(instruction.getInputNames().get(0));
-                input2Register = ARMap.findRegisterWithName(instruction.getInputNames().get(1));
+                input1Register = ARMap.findRegisterWithName(instruction.getParameters()[1]);
+                input2Register = ARMap.findRegisterWithName(instruction.getParameters()[2]);
+
 
                 // According to the textbook section 2.2 table 2.2.2 this command works:
                 // subtract is	SUB X1, X2, X3 which means this in human readable form X1 = X2 - X3
@@ -28,32 +29,35 @@ public class DecimalInstructionTranslationServiceImpl implements InstructionTran
                 destinationRegister.setValue(input1Register.getValue() - input2Register.getValue());
                 break;
             case "ADDI":
-                input1Register = ARMap.findRegisterWithName(instruction.getInputNames().get(0));
-                Integer immediateValue = Integer.parseInt(instruction.getInputNames().get(1));
-                destinationRegister.setValue(input1Register.getValue() - immediateValue);
-                break;
-            case "SUBI":
-                input1Register = ARMap.findRegisterWithName(instruction.getInputNames().get(0));
-                immediateValue = Integer.parseInt(instruction.getInputNames().get(1));
+                input1Register = ARMap.findRegisterWithName(instruction.getParameters()[1]);
+                Integer immediateValue = Integer.parseInt(instruction.getParameters()[2]);
                 destinationRegister.setValue(input1Register.getValue() + immediateValue);
                 break;
+            case "SUBI":
+                input1Register = ARMap.findRegisterWithName(instruction.getParameters()[1]);
+                immediateValue = Integer.parseInt(instruction.getParameters()[2]);
+                destinationRegister.setValue(input1Register.getValue() - immediateValue);
+                break;
             case "LDUR":
-                int memoryAddress = getMemoryAddress(instruction.getInputNames());
+                int memoryAddress = getMemoryAddress(instruction.getParameters());
                 destinationRegister.setValue(ARMap.memory[memoryAddress]);
                 break;
             case "STUR":
-                memoryAddress = this.getMemoryAddress(instruction.getInputNames());
+                memoryAddress = this.getMemoryAddress(instruction.getParameters());
                 ARMap.memory[memoryAddress] = destinationRegister.getValue();
                 break;
+            case "MOVZ":
+                immediateValue = Integer.parseInt(instruction.getParameters()[1]);
+                destinationRegister.setValue(immediateValue);
         }
 
     }
 
     // Gets the memory address from code set up as [X2, 3] where the first part is the
     // name of a register and the second is an offeset for memory. This function allows for DRY
-    private int getMemoryAddress(List<String> inputList) {
-        Register baseRegister = ARMap.findRegisterWithName(inputList.get(0).substring(1));
-        String offsetString = inputList.get(1);
+    private int getMemoryAddress(String[] parameters) {
+        Register baseRegister = ARMap.findRegisterWithName(parameters[1].substring(1));
+        String offsetString = parameters[2];
         int offset = Integer.parseInt(offsetString.substring(0, offsetString.length()-1));
         return baseRegister.getValue() + offset;
     }
--- app/src/main/java/ekordik/cuw/edu/arminterpreter/Instruction.java
+++ app/src/main/java/ekordik/cuw/edu/arminterpreter/Instruction.java
@@ -4,29 +4,25 @@ import java.util.LinkedList;
 
 public class Instruction {
     private String magicTrick;
-    private String destinationName;
-    private LinkedList<String> inputNames = new LinkedList<>();
+    private String[] parameters;
 
     public Instruction(String instruction) {
         instruction = instruction.trim();
         int locationOfFirstSpace = instruction.indexOf(' ');
         this.magicTrick = instruction.substring(0, locationOfFirstSpace);
         instruction = instruction.substring(locationOfFirstSpace).trim();
-        String[] parts = instruction.split(",");
-        this.destinationName = parts[0].trim();
-        this.inputNames.add(parts[1].trim());
-        this.inputNames.add(parts[2].trim());
+        this.parameters = instruction.split(",");
+        for(int i = 0; i < this.parameters.length; i++) {
+            this.parameters[i] = this.parameters[i].trim();
+        }
     }
 
     public String getMagicTrick() {
         return magicTrick;
     }
 
-    public String getDestinationName() {
-        return destinationName;
+    public String[] getParameters() {
+        return this.parameters;
     }
 
-    public LinkedList<String> getInputNames() {
-        return inputNames;
-    }
 }
